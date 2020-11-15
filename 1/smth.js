7b90537e7489e53890f2527bafccab3946d078c7
Pavel <70834800+ImLemmiwinks@users.noreply.github.com>
2020-11-15 14:33:54 +0400

    Update smth.js

--- 1/smth.js
+++ 1/smth.js
@@ -1,14 +1,14 @@
 /*
-let goods = [{id: 1, name: "Цельнометаллическая шестеренка: Отмщение", textPrice: "550 руб.", price: 550},
+var goods = [{id: 1, name: "Цельнометаллическая шестеренка: Отмщение", textPrice: "550 руб.", price: 550},
 			 {id: 2, name: "Аид", textPrice: "450 руб.", price: 450},
 			 {id: 3, name: "Врата Балдура 3", textPrice: "1800 руб.", price: 1800}
 			];
 */
 
 var counter = 0;
-let goodsCounterElement = document.getElementById('counter');
-let itemsList = document.getElementById('itemsList');
-let ids = [];
+var goodsCounterElement = document.getElementById('counter');
+var itemsList = document.getElementById('itemsList');
+var ids = [];
 var items = [];
 
 
