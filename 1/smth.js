5bbfb9475d47779ee93f1bc1a13dea579ea26793
Pavel <70834800+ImLemmiwinks@users.noreply.github.com>
2020-11-15 14:04:33 +0400

    Add smth.js

---
+++ 1/smth.js
@@ -0,0 +1,33 @@
+/*
+let goods = [{id: 1, name: "Цельнометаллическая шестеренка: Отмщение", textPrice: "550 руб.", price: 550},
+			 {id: 2, name: "Аид", textPrice: "450 руб.", price: 450},
+			 {id: 3, name: "Врата Балдура 3", textPrice: "1800 руб.", price: 1800}
+			];
+*/
+
+var counter = 0;
+let goodsCounterElement = document.getElementById('counter');
+let itemsList = document.getElementById('itemsList');
+let ids = [];
+var items = [];
+
+
+function addItemToCart(id, title, price) {
+	counter++;
+	goodsCounterElement.innerText = counter;
+	
+	items.push({
+		id: id,
+		title: title,
+		price: price
+	});
+	itemsList.innerHTML = items;
+}
+
+function popup() {
+  document.querySelector("div.items-list-background").style.visibility = 'visible';
+}
+
+function popupQuit() {
+  document.querySelector("div.items-list-background").style.visibility = 'hidden';
+}
