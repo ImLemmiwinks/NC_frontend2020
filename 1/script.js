df86fa8fe179146038ca7ddfb3cd82c9d393e9cd
Pavel <70834800+ImLemmiwinks@users.noreply.github.com>
2020-11-17 20:49:57 +0400

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -1,46 +1,47 @@
 /*
-var goods = [{id: 1, name: "Цельнометаллическая шестеренка: Отмщение", textPrice: "550 руб.", price: 550},
+const goods = [{id: 1, name: "Цельнометаллическая шестеренка: Отмщение", textPrice: "550 руб.", price: 550},
 			 {id: 2, name: "Аид", textPrice: "450 руб.", price: 450},
 			 {id: 3, name: "Врата Балдура 3", textPrice: "1800 руб.", price: 1800}
 			];
 */
 
-var counter = 0;
-var goodsCounterElement = document.getElementById('counter');
-var itemsList = document.getElementById('itemsList');
-var ids = [];
-var items = [];
+let counter = 0;
+const goodsCounterElement = document.getElementById('counter');
+const itemsList = document.getElementById('itemsList');
+let items = new Map();
 
-var quantity = 0;
-
-let itemBlock = document.createElement('div');
-itemBlock.className = 'item-block'
 
 function addItemToCart(id, title, price) {
 	counter++;
 	goodsCounterElement.innerText = counter;
-	
-	items.push({
-		id: id,
+	if (items.has(id)) {
+		items.get(id).quantity++;
+	} else {
+		items.set(id, {
 		title: title,
 		price: price,
-	});
-	if (ids.includes(id)) {
-		quantity++
-	} else {
-		ids.push(id);	
-		let itemBlock = document.createElement('div');
-		itemBlock.className = 'item-block'
-		itemBlock.id = 'Game'+id;
-		itemsList.appendChild (itemBlock);
-		itemBlock.innerHTML = title + ' ' + price;
+		quantity: 1,
+		});
+
 	};
 }
 
-function popup() {
-  document.querySelector("div.items-list-background").style.visibility = 'visible';
+function openPopup() {
+	let template = ``;
+	items.forEach(({title, price, quantity})=>{
+		template += `
+		<div class="item-block">
+			<span>${title}</span>
+			<span>За 1 шт: ${price} руб.</span>
+			<span>Кол-во: ${quantity}</span>
+			<span>Общая цена: ${price*quantity} </span>
+		</div>
+		`;
+	});
+	itemsList.innerHTML = template;
+	document.querySelector(".overlay").style.visibility = 'visible';
 }
 
-function popupQuit() {
-  document.querySelector("div.items-list-background").style.visibility = 'hidden';
+function quitPopup() {
+  document.querySelector(".overlay").style.visibility = 'hidden';
 }
